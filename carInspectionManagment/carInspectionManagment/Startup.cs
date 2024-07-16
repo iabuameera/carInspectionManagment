9842867ed8342eed8b113543679459976ef58d3a
iabuameera <iabuameera@asaltech.com>
2024-07-16 13:52:57 +0300

    add GRPC

--- carInspectionManagment/carInspectionManagment/Startup.cs
+++ carInspectionManagment/carInspectionManagment/Startup.cs
@@ -1,23 +1,15 @@
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
 using CarInspectionManagment.Business.Infrastructure;
-using CarInspectionManagment.Business.Managers;
-using CarInspectionManagment.Business.Persistence.Repositories;
+using CarInspectionManagment.Business.Persistence;
 using ConncterLayer;
 using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 
 namespace carInspectionManagment
@@ -76,10 +68,6 @@ namespace carInspectionManagment
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
         }
     }
 
